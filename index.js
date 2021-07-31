73d82564a57e83bbc2566bf1f8fd67fb4a035fb2
not-a-scam <zechariahyeo02@gmail.com>
2021-07-31 12:33:01 +0800

    complete active states

--- index.js
+++ index.js
@@ -10,3 +10,23 @@ function dropMenu(){
     return false;
 }
 
+const learnMoreLinks = document.getElementsByClassName("learn-more");
+for(let i = 0; i < learnMoreLinks.length; i++){
+    let link = learnMoreLinks[i].firstChild;
+    if(link.classList.contains("yellow")){
+        learnMoreLinks[i].addEventListener("mouseover" ,() => {
+            link.style.backgroundColor = "hsl(51, 100%, 49%)";
+        });
+        learnMoreLinks[i].addEventListener("mouseout" ,() => {
+            link.style.backgroundColor = "hsla(51, 100%, 49%, 0.288)";
+        });
+    } else if(link.classList.contains("red")){
+        learnMoreLinks[i].addEventListener("mouseover" ,() => {
+            link.style.backgroundColor = "hsl(7, 99%, 70%)";
+        });
+        learnMoreLinks[i].addEventListener("mouseout" ,() => {
+            link.style.backgroundColor = "hsla(7, 99%, 70%, 0.212)";
+        });
+    }
+
+}
